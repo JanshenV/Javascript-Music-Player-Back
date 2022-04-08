e60e10e438995751ce55eaa71498e1fa05d55859
Janshen Vernon <janshen.vbb@gmail.com>
2022-04-08 11:52:22 -0300

    changed secondvalidation's place for clearer code on controllers

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -8,31 +8,7 @@ const {
     yupUserLogin,
     yupUserEdit
 } = require('../validations/yupUser');
-
-async function SecondValidation(email, password) {
-    let existingEmail = await knex('users')
-        .where({ email })
-        .first();
-
-    if (existingEmail) {
-        return { existingEmail };
-    };
-
-    //Password must be min 9 characters. One being upperCase, another lowerCaser and a special character
-    const strongPassword = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$&*]).{9,}$/;
-    if (!strongPassword.test(password)) {
-        let validateResponse = `
-            Senha deve contar ao mínimo um dígito numérico.
-            Senha deve ter ao mínimo um dígito em caixa baixa.
-            Senha deve ter ao mínimo um dígito em caixa alta.
-            Senha deve ter conter no mínimo um caractere especial.
-            `.trim();
-        return { validateResponse };
-    } else {
-        const encryptedPassword = await bcrypt.hash(String(password), 10);
-        return { encryptedPassword }
-    };
-};
+const SecondValidation = require('../validations/secondValidation');
 
 async function EmailWithNodemailer(email, username) {
     const data = {
@@ -198,8 +174,6 @@ async function UserEdit(req, res) {
         })
     }
 };
-
-
 
 module.exports = {
     CreateUser,
---
+++ src/validations/secondValidation.js
@@ -0,0 +1,30 @@
+const knex = require('../database/connection');
+
+
+async function SecondValidation(email, password) {
+    let existingEmail = await knex('users')
+        .where({ email })
+        .first();
+
+    if (existingEmail) {
+        return { existingEmail };
+    };
+
+    //Password must be min 9 characters. One being upperCase, another lowerCaser and a special character
+    const strongPassword = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$&*]).{9,}$/;
+    if (!strongPassword.test(password)) {
+        let validateResponse = `
+            Senha deve contar ao mínimo um dígito numérico.
+            Senha deve ter ao mínimo um dígito em caixa baixa.
+            Senha deve ter ao mínimo um dígito em caixa alta.
+            Senha deve ter conter no mínimo um caractere especial.
+            `.trim();
+        return { validateResponse };
+    } else {
+        const encryptedPassword = await bcrypt.hash(String(password), 10);
+        return { encryptedPassword }
+    };
+};
+
+
+module.exports = SecondValidation;
